c7443bd816efba33db9334b7189a4d80d7f32f2d
Ruben Hernandez <rubendez21@gmail.com>
2024-09-12 21:51:15 -0600

    Changes for assignment 4 part 2
    Signed-off-by: Ruben Hernandez <rubendez21@gmail.com>

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -5,9 +5,9 @@
 set -e
 set -u
 
-NUMFILES=10
+NUMFILES=1
 WRITESTR=AELD_IS_FUN
-WRITEDIR=/tmp/aeld-data
+WRITEDIR=/tmp/
 username=$(cat conf/username.txt)
 
 if [ $# -lt 3 ]
@@ -32,7 +32,8 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+#assignment=`cat conf/assignment.txt`
+assignment=`cat /etc/finder-app/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -56,10 +57,11 @@ for i in $( seq 1 $NUMFILES)
 do
 	make clean
 	#make
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	/bin/writer "$WRITEDIR/assignment-4-result.txt" "$WRITESTR"
+	#./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(/bin/finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
